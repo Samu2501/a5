37f2faec4648e92615d2a045eb56ea50027ffc48
SAMUEL KLAR <SAMUEL.KLAR@gmx.de>
2025-05-21 14:12:58 +0200

    mit Gleitzahlen rechnen erlaubt

---
+++ Operator.java
@@ -0,0 +1,12 @@
+
+/**
+ * Interface Operator
+ * 
+ * @author Malte Nagel, Prof. Dr.-Ing. Emre Cakar
+ * @FunctionalInterface
+ */
+
+public interface Operator
+{
+    double calculate(double a, double b);
+}
---
+++ Rechner.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+/**
+ * Ein Taschenrechner
+ * 
+ * @author Malte Nagel, Prof. Dr.-Ing. Emre Cakar
+ */
+public class Rechner
+{
+
+    /*
+     * Die main-Methode. Gibt "Hallo Welt!" aus.
+     */
+    public static void main(String[] args)
+    {
+        // Einrichtung
+        double a, b, result = 0;
+        String op;
+        JConsole console = new JConsole();
+        
+        Operator add = (x, y) -> x + y;
+        Operator sub = (x, y) -> x - y;
+        
+        while (true) {
+            // Eingabe
+            do {
+                op = console.readString("Welche Operation soll durchgeführt werden (+,-)?: ");
+            } while(!op.equals("+") && !op.equals("-"));
+            
+            a = console.readInt("Erste Ganzzahl: ");
+            b = console.readInt("Zweite Ganzzahl: ");
+            
+            // Verarbeitung
+            if (op.equals("+")) {
+                result = add.calculate(a, b);
+            } else if (op.equals("-")) {
+                result = sub.calculate(a, b);
+            }
+            
+            // Ausgabe
+            console.println("Ergebnis: " + result);
+        }
+    }
+
+}
